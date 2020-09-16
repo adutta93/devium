9e129498c8ff77a171dfc06b07859afca48e4312
Akash Dutta <dutta.akash1993official@gmail.com>
2020-09-16 12:10:23 +0530

    all previous front end deleted

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Suspense } from "react";
 import "./App.css";
 import Loader from "./Components/Loader";
-import AppRouter from "./Router/";
+
 function App() {
   return (
     <div className="App">
       <Suspense fallback={<Loader />}>
-        <AppRouter />
+        
       </Suspense>
     </div>
   );
