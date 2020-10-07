84ec79854cc6845ccb56fe90f1c5af690a3bef8f
Akash Dutta <dutta.akash1993official@gmail.com>
2020-10-07 12:05:45 +0530

    some changes

--- controller/authController.js
+++ controller/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const validateLoginInput = require("../utils/validation/login");
 // Verify user
 exports.verify = async (req, res) => {
   try {
@@ -17,49 +18,101 @@ exports.verify = async (req, res) => {
 };
 
 // LOG IN
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    //validation email and password
-    if (!email || !password) {
-      return res.status(400).json({
-        Err: [{ msg: "Please enter mail id or password" }],
-      });
-    }
 
-    //check for user
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-      return res.status(401).json({
-        err: [{ msg: "Invalid user" }],
-      });
-    }
+// exports.login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     //validation email and password
+//     if (!email || !password) {
+//       return res.status(400).json({
+//         Err: [{ msg: "Please enter mail id or password" }],
+//       });
+//     }
 
-    //check for password
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({
-        err: [{ mag: "Invalid Password" }],
-      });
-    }
+//     //check for user
+//     const user = await User.findOne({ email }).select("+password");
+//     if (!user) {
+//       return res.status(401).json({
+//         err: [{ msg: "Invalid user" }],
+//       });
+//     }
 
-    //if everything matches
-    const payload = {
-      user: {
-        id: user.id,
-        email: user.email,
-      },
-    };
-    const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
-      expiresIn: "5h",
-    });
+//     //check for password
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res.status(401).json({
+//         err: [{ mag: "Invalid Password" }],
+//       });
+//     }
 
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({
-      Error: "Unable to log in!",
-    });
+//     //if everything matches
+//     const payload = {
+//       user: {
+//         id: user.id,
+//         email: user.email,
+//       },
+//     };
+//     const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
+//       expiresIn: "5h",
+//     });
+
+//     res.status(200).json({ token });
+//   } catch (error) {
+//     res.status(500).json({
+//       Error: "Unable to log in!",
+//     });
+//   }
+// };
+
+//login2
+
+exports.login = (req, res) => {
+  //Form Valdiation
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
   }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by Email
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+        };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          process.env.TOKEN_SECRET,
+          {
+            expiresIn: "5h",
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
 };
 
 // SIGNOUT
--- controller/userController.js
+++ controller/userController.js
@@ -6,57 +6,103 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 require("dotenv").config();
 
-exports.registration = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      errors: errors.array(),
-    });
+const validateRegisterInput = require("../utils/validation/register");
+const validateLoginInput = require("../utils/validation/login");
+
+// exports.registration = async (req, res) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({
+//       errors: errors.array(),
+//     });
+//   }
+
+//   const { name, email, password } = req.body;
+//   try {
+//     let user = await User.findOne({ email });
+//     if (user) {
+//       return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+//     }
+
+//     const avatar = gravatar.url(email, {
+//       s: "200",
+//       r: "pg",
+//       d: "mm",
+//     });
+
+//     user = new User({
+//       name,
+//       email,
+//       avatar,
+//       password,
+//     });
+
+//     const salt = await bcrypt.genSalt(10);
+//     user.password = await bcrypt.hash(password, salt);
+//     await user.save();
+
+//     const payload = {
+//       user: {
+//         id: user.id,
+//       },
+//     };
+
+//     jwt.sign(
+//       payload,
+//       process.env.TOKEN_SECRET,
+//       { expiresIn: "5h" },
+//       (err, token) => {
+//         if (err) {
+//           throw err;
+//         }
+//         res.status(200).json({ token });
+//       }
+//     );
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send("Server error");
+//   }
+// };
+
+// register2
+
+exports.registration = (req, res) => {
+  //Form validation
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
   }
 
-  const { name, email, password } = req.body;
-  try {
-    let user = await User.findOne({ email });
+  User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        avatar,
+      });
+
+      // Hash password before storing in database
+      const rounds = 10;
+      bcrypt.genSalt(rounds, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
     }
-
-    const avatar = gravatar.url(email, {
-      s: "200",
-      r: "pg",
-      d: "mm",
-    });
-
-    user = new User({
-      name,
-      email,
-      avatar,
-      password,
-    });
-
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
-    await user.save();
-
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.TOKEN_SECRET,
-      { expiresIn: "5h" },
-      (err, token) => {
-        if (err) {
-          throw err;
-        }
-        res.status(200).json({ token });
-      }
-    );
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server error");
-  }
+  });
 };
